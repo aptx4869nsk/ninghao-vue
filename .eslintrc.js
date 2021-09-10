69cfe872ebf69dc6f743c8b0353683d4ad63e4b6
Nang San Kham <nangsanthinzarlwin@gmail.com>
2021-09-10 16:02:53 +0800

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,24 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
-  rules: {
+  // Vue default rules
+  // rules: {
+  //   'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  //   'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  // }
+  "rules": {
+    // disable the rule for all files
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+  },
+  "overrides": [
+    {
+      // enable the rule specifically for TypeScript files
+      "files": ["*.ts", "*.tsx"],
+      "rules": {
+        "@typescript-eslint/explicit-module-boundary-types": ["error"]
+      }
+    }
+  ]
 }
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue';
+import App from './app/app.vue';
+
+/**
+ * 創建應用
+ */
+const app = createApp(App);
+
+/**
+ * 挂載應用
+ */
+
+app.mount('#app');
